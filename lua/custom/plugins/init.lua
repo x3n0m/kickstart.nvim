33c7bbde09c64f1b99ea67eeadae3ec687b81dee
x3m0m <rrohith958@gmail.com>
2026-04-06 08:44:43 +0530

    added my changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,7 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+<<<<<<< Updated upstream
 
 ---@module 'lazy'
 ---@type LazySpec
+=======
+vim.keymap.set('n', ';', ':')
+vim.keymap.set('i', 'jj', '<ESC>')
+vim.opt.clipboard = 'unnamedplus'
+
+-- Navigate splits with Ctrl+hjkl
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+>>>>>>> Stashed changes
 return {}
